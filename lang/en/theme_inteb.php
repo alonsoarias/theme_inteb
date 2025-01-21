d38f30024530e5c2e1180d43a722b4bed8f4d538
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-01-20 22:24:24 -0500

    Update theme_inteb.php

--- lang/en/theme_inteb.php
+++ lang/en/theme_inteb.php
@@ -1,129 +1,136 @@
 <?php
 $string['pluginname'] = 'INTEB';
-$string['choosereadme'] = 'INTEB is a theme created by Ing. Pablo A Pico (<a href="http://ingeweb.co/">IngeWeb</a>) exclusively for our customers.';
+$string['choosereadme'] = 'INTEB is a theme created by Eng. Pablo A Pico (<a href="http://ingeweb.co/">IngeWeb</a>) exclusively for our clients.';
 $string['configtitle'] = 'INTEB';
 
 $string['logo'] = 'Logo';
 
-$string['themesettings'] = 'Theme Settings INTEB';
-$string['themesettingsgeneral'] = 'General Sett -topic Settings';
-$string['contactinfo'] = 'Contact info';
+$string['themesettings'] = 'INTEB Theme Settings';
+$string['themesettingsgeneral'] = 'General Settings';
+$string['contactinfo'] = 'Contact Information';
 $string['abouttitle'] = 'About';
 
 $string['abouttext'] = 'About Text';
-$string['abouttextdesc'] = 'Content for "About" in footer area';
+$string['abouttextdesc'] = 'Content for the "About" section in the footer';
 
 $string['loginfootertext'] = 'Login Footer';
-$string['loginfootertextdesc'] = 'Content for footer area in login page';
+$string['loginfootertextdesc'] = 'Content for the footer section on the login page';
 
 $string['generalalert'] = 'General Alert';
-$string['generalalertdesc'] = 'This will display an alert on your frontpage to advise users of a particular event/situation.';
+$string['generalalertdesc'] = 'This will display an alert on the main page to notify users about a particular event or situation.';
 
-$string['footertitle'] = 'Middle Footer Title';
-$string['footertitledesc'] = 'The title for the middle module in the footer area.';
+$string['footertitle'] = 'Footer Center Block Title';
+$string['footertitledesc'] = 'The title for the center block in the footer.';
 
-$string['footertext'] = 'Middle Footer Text';
-$string['footertextdesc'] = 'The content for the middle module in the footer area.';
+$string['footertext'] = 'Footer Center Block Text';
+$string['footertextdesc'] = 'The content for the center block in the footer.';
 
-$string['footertitle2'] = 'Right Footer Title';
-$string['footertitledesc2'] = 'The title for the right module in the footer area.';
+$string['footertitle2'] = 'Footer Right Block Title';
+$string['footertitledesc2'] = 'The title for the right block in the footer.';
 
-$string['footertext2'] = 'Right Footer Text';
-$string['footertextdesc2'] = 'The content for the right module in the footer area.';
+$string['footertext2'] = 'Footer Right Block Text';
+$string['footertextdesc2'] = 'The content for the right block in the footer.';
 
 $string['copyright'] = 'Copyright';
 $string['copyrightdesc'] = 'About this website.';
 
 $string['customcss'] = 'Custom CSS';
-$string['customcssdesc'] = 'Whatever CSS rules you add to this area will be reflected in every page, making for easier customisation of this theme.';
+$string['customcssdesc'] = 'Any CSS rules you add here will be reflected on every page, making it easy to customize this theme.';
 
-$string['themeinfotext'] = 'This theme was created for <strong> another Moodle project </strong> by <a target="_blank" href="http://ingeweb.co/">IngeWeb - Solutions for Success on the Internet</a>.';
-$string['credit'] = 'Theme created by <a target="_blank" class="my-credit-link" href="http://ingeweb.co/">IngeWeb</a> Copyright © 2024 - All rights reserved';
+$string['themeinfotext'] = 'This theme was created for <strong>another Moodle project</strong> by <a target="_blank" href="http://ingeweb.co/">IngeWeb - Solutions to succeed on the Internet</a>.';
+$string['credit'] = 'Theme created by <a target="_blank" class="my-credit-link" href="http://ingeweb.co/">IngeWeb</a> © 2024 - All rights reserved';
 
 $string['loginimage'] = 'Welcome Image';
-$string['loginimagedesc'] = 'This image appears in the login page. 960px * 960px PNG or JPG is recommended.<br><strong>Examples</strong>';
+$string['loginimagedesc'] = 'This image appears on the login page. PNG or JPG of 960px * 960px is recommended.<br><strong>Examples</strong>';
 
 $string['loginbg_color'] = 'Login Background Color';
-$string['loginbg_colordesc'] = 'Background color in login page';
+$string['loginbg_colordesc'] = 'Background color on the login page';
 
-$string['frontpagewallpaper'] = 'Frontpage wallpaper';
-$string['frontpagewallpaperdesc'] = 'This image appears in the top of the frontpage right after the main menu.';
+$string['frontpagewallpaper'] = 'Front Page Background Image';
+$string['frontpagewallpaperdesc'] = 'This image appears at the top of the front page, right after the main menu.';
 
-$string['showingonlycategorieswithenrolledcourses'] = 'Showing categories containing enrolled courses only';
-$string['nocoursestoshow'] = 'No courses to show were found';
+$string['showingonlycategorieswithenrolledcourses'] = 'Showing only categories with enrolled courses';
+$string['nocoursestoshow'] = 'No courses found to display';
 
 $string['region-side-pre'] = 'Right';
 
 // General notice
-$string['generalnoticemode'] = 'General notice mode';
-$string['generalnoticemodedesc'] = 'The general notice was created to allow administrators to show important notices in the header in every page after logging in.';
+$string['generalnoticemode'] = 'General Notice Mode';
+$string['generalnoticemodedesc'] = 'The general notice allows administrators to display important notifications in the header of each page after login.';
 $string['generalnoticemode_off'] = 'Off';
-$string['generalnoticemode_info'] = 'Info';
+$string['generalnoticemode_info'] = 'Information';
 $string['generalnoticemode_danger'] = 'Critical';
 
-$string['generalnotice'] = 'Notice text';
-$string['generalnoticedesc'] = 'Text to show inside the alert box. You may use <strong>strong</strong> tag for important phrases in this text.';
-$string['generalnotice_create'] = 'Set a notice';
+$string['generalnotice'] = 'Notice Text';
+$string['generalnoticedesc'] = 'Text to be displayed inside the alert box. You can use the <strong>strong</strong> tag to highlight important phrases.';
+$string['generalnotice_create'] = 'Configure a Notice';
 
 // Cache
-$string['cachedef_fontawesomeremuiiconmapping'] = 'Cache Storage for Font Awesome';
-
-// Removed old copy/paste strings (no longer needed).
+$string['cachedef_fontawesomeremuiiconmapping'] = 'Cache storage for Font Awesome icon mapping';
 
 $string['show'] = 'Show';
 $string['hide'] = 'Hide';
 
 $string['carouselsettings'] = 'Carousel Settings';
-$string['carouselsettings_desc'] = 'Configure the settings for the login page carousel including images, texts, and other options.';
+$string['carouselsettings_desc'] = 'Configure the settings for the carousel on the login page, including images, texts, and other options.';
 
-$string['enablecarousel'] = 'Enable login page carousel';
-$string['enablecarousel_desc'] = 'Enable this setting to display a carousel of images on the login page.';
+$string['enablecarousel'] = 'Enable carousel on login page';
+$string['enablecarousel_desc'] = 'Enable this setting to display an image carousel on the login page.';
 $string['numberofslides'] = 'Number of slides';
-$string['numberofslides_desc'] = 'Select the number of slides to display in the carousel on the login page.';
-$string['slideimage'] = 'Slide {$a} image';
+$string['numberofslides_desc'] = 'Select how many slides you want to show in the login page carousel.';
+$string['slideimage'] = 'Slide Image {$a}';
 $string['slideimage_desc'] = 'Upload an image for slide {$a} in the carousel.';
 $string['carouselinterval'] = 'Carousel Interval';
-$string['carouselintervaldesc'] = 'Enter the interval time in milliseconds for the carousel slides.';
+$string['carouselintervaldesc'] = 'Enter the interval time in milliseconds for carousel slides.';
 
-$string['carousel'] = 'Image carousel';
-$string['loginsection'] = 'Login section';
+$string['carousel'] = 'Image Carousel';
+$string['loginsection'] = 'Login Section';
 
 $string['slidetitle'] = 'Title for slide {$a}';
 $string['slidetitle_desc'] = 'Enter the title for slide {$a}';
-$string['slideurl'] = 'Slide {$a} Button URL';
-$string['slideurldesc'] = 'Enter the URL for the button on slide {$a}. This URL will open in a new window.';
+$string['slideurl'] = 'Button URL in slide {$a}';
+$string['slideurldesc'] = 'Enter the URL for the button in slide {$a}. This URL will open in a new window.';
 
 $string['unauthorized_access'] = 'Unauthorized Access';
-$string['unauthorized_access_msg'] = 'This site is not authorized to operate at this URL.';
-$string['devtools_access_disabled'] = 'Access to the development tools is disabled.';
+$string['unauthorized_access_msg'] = 'This site is not authorized to operate on this URL.';
+$string['devtools_access_disabled'] = 'Access to development tools has been disabled.';
 
-$string['hidefrontpagesections'] = 'Hide front page top sections';
-$string['hidefrontpagesections_desc'] = 'Enable this setting to hide the top sections of your front page, providing a cleaner look.';
+$string['personalareaheader'] = 'Personal Area Header Image';
+$string['personalareaheaderdesc'] = 'Upload an image that will be displayed as header in the personal area.';
+$string['showpersonalareaheader'] = 'Show Personal Area Header Image';
+$string['showpersonalareaheader_desc'] = 'Enable this setting to show a header image in the personal area.';
+$string['mycoursesheader'] = 'My Courses Header Image';
+$string['mycoursesheaderdesc'] = 'Upload an image that will be displayed as header on the My Courses page.';
+$string['showmycoursesheader'] = 'Show My Courses Header Image';
+$string['showmycoursesheader_desc'] = 'Enable this setting to show a header image on the My Courses page.';
+$string['hidefrontpagesections'] = 'Hide Front Page Top Sections';
+$string['hidefrontpagesections_desc'] = 'Enable this setting to hide the top sections of the front page, providing a cleaner look.';
+$string['hidefootersections'] = 'Hide Footer Sections';
+$string['hidefootersections_desc'] = 'Enable this setting to hide footer sections, providing a cleaner appearance.';
 
-$string['themesettingslogin'] = 'Basic login settings';
+$string['themesettingslogin'] = 'Basic Login Settings';
 
 $string['abouttext_default'] = '<p style="text-align: center;"></p>
     <p style="text-align: left;">
     </p>
-    <p style="text-align: center;"><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><b>IngeWeb - Solutions for Success on the Internet<br></b></span><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">Experts in Moodle, BigBlueButton, WordPress, and Joomla.<br></span><strong style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">www.ingeweb.co</strong></p>
+    <p style="text-align: center;"><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;"><b>IngeWeb - Solutions to succeed on the Internet<br></b></span><span style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">Experts in Moodle, BigBlueButton, WordPress and Joomla.<br></span><strong style="font-family: inherit; font-size: 1rem; letter-spacing: 0rem; text-transform: inherit;">www.ingeweb.co</strong></p>
     <p></p>';
 
-// New strings for Chat
-$string['themesettingschat'] = 'Chat settings';
-$string['themesettingschatdesc'] = 'Options for integrating a chat service into your site.';
+// Chat strings
+$string['themesettingschat'] = 'Chat Settings';
+$string['themesettingschatdesc'] = 'Options to integrate a chat service into the platform.';
 $string['enable_chat'] = 'Enable chat';
-$string['enable_chatdesc'] = 'If you enable this, a chat widget will be displayed.';
-$string['tawkto_embed_url'] = 'Tawk.to embed URL';
-$string['tawkto_embed_urldesc'] = 'Enter your Tawk.to (or another chat service) embed script or URL.';
+$string['enable_chatdesc'] = 'If you enable this option, a chat widget will be displayed.';
+$string['tawkto_embed_url'] = 'Tawk.to Embed URL';
+$string['tawkto_embed_urldesc'] = 'Enter the Tawk.to (or other chat service) embed script or URL.';
 
-// New strings for Copy/Paste
+// Copy/Paste strings
 $string['themesettingscopypaste'] = 'Copy/Paste Settings';
-$string['themesettingscopypaste_desc'] = 'Options to block or restrict copy/paste.';
+$string['themesettingscopypaste_desc'] = 'Options to block or restrict copying and pasting.';
 $string['copypaste_prevention'] = 'Prevent copy/paste';
-$string['copypaste_preventiondesc'] = 'If enabled, attempts to block the copy/paste function.';
-$string['copypaste_roles'] = 'Roles affected';
-$string['copypaste_rolesdesc'] = 'Select the roles to which the restriction applies.';
-
+$string['copypaste_preventiondesc'] = 'If enabled, it will attempt to block the copy and paste function.';
+$string['copypaste_roles'] = 'Affected roles';
+$string['copypaste_rolesdesc'] = 'Select the roles to which the restriction will apply.';
 
 $string['intebhelp'] = "Platform Help";
-$string['intebsupport'] = "IngeWeb Support";
+$string['intebsupport'] = "Inteb Support";
