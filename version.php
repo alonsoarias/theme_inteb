8ae3a53c76291708cd5217719fab39533d3936ef
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2025-01-20 23:29:48 -0500

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
  defined('MOODLE_INTERNAL') || die();
  
  $plugin->component = 'theme_inteb';
- $plugin->version = 2024042504; // Fecha de la versión: Año, mes, día, incremento.
+ $plugin->version = 2024042506; // Fecha de la versión: Año, mes, día, incremento.
  $plugin->requires = 2020061500; // Versión mínima de Moodle requerida.
  $plugin->dependencies = [
      'theme_remui' => 2023040700, // Dependencia del tema padre.
